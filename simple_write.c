ae46584f438adab45967c40e6ae5b959daa49d8d
Ron Guilmet <ronpguilmet@gmail.com>
2020-08-10 14:35:52 -0400

    cleaning up

--- simple_write.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include <stdlib.h>
-
-int main(){
-    if ((write(1, "Hello Ron\n", 10)) != 10)
-        write(2, "A write error has occurred on file descriptor 1\n",10);
-    exit(0);
-}
